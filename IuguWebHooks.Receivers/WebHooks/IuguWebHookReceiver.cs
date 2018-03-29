6628372f4d03d24ccd6daf3d907f6fcbc0207f1c
Haruno Kenobi <arthur.souza@sharepro.com.br>
2018-03-29 14:51:14 -0300

    Implementing a secure validation with the token 'IubuWebHook_SecretKey' getting the basic authorization from header of request provided by Iugu.

--- IuguWebHooks.Receivers/WebHooks/IuguWebHookReceiver.cs
+++ IuguWebHooks.Receivers/WebHooks/IuguWebHookReceiver.cs
@@ -3,8 +3,12 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Configuration;
+using System.Globalization;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 
 namespace IuguWebHooks.Receivers
@@ -15,6 +19,8 @@ namespace IuguWebHooks.Receivers
 
         internal const string ReceiverName = "iugu";
         internal const string DefaultAction = "none";
+
+        internal const string ConfigToken = "IubuWebHook_SecretKey";
 
 
 
@@ -40,7 +46,9 @@ namespace IuguWebHooks.Receivers
             }
 
             // Ensure that we use https and have a valid code parameter
-            await EnsureValidCode(request, id);
+            //await EnsureValidCode(request, id);
+
+            this.EnsureValidBasicAuthenticationToken(request);
 
             // Read the request entity body
             var data = await ReadAsFormDataAsync(request);
@@ -68,6 +76,58 @@ namespace IuguWebHooks.Receivers
             }
 
             return JsonConvert.SerializeObject(dictionary, Formatting.None);
+        }
+
+        /// <summary>
+        /// Method based on [Microsoft.AspNet.WebHooks.WebHookReceiver.EnsureValidCode] method
+        /// </summary>
+        /// <param name="request"></param>
+        private void EnsureValidBasicAuthenticationToken(HttpRequestMessage request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            this.EnsureSecureConnection(request);
+
+            var secretKeyRequested = request.Headers?.Authorization?.Scheme ?? string.Empty;
+
+            if (string.IsNullOrEmpty(secretKeyRequested))
+            {
+                var message = string.Format(CultureInfo.CurrentCulture, "The WebHook verification request must contain a '{0}' query parameter.", ConfigToken);
+                request.GetConfiguration().DependencyResolver.GetLogger().Error(message);
+                var noCode = request.CreateErrorResponse(HttpStatusCode.BadRequest, message);
+                throw new HttpResponseException(noCode);
+            }
+
+            var secretKey = this.GetReceiverConfig(request);
+
+            if (!WebHookReceiver.SecretEqual(secretKeyRequested, secretKey))
+            {
+                var message = string.Format(CultureInfo.CurrentCulture, "The '{0}' query parameter provided in the HTTP request did not match the expected value.", ConfigToken);
+                request.GetConfiguration().DependencyResolver.GetLogger().Error(message);
+                var invalidCode = request.CreateErrorResponse(HttpStatusCode.BadRequest, message);
+                throw new HttpResponseException(invalidCode);
+            }
+        }
+
+        public string GetReceiverConfig(HttpRequestMessage request)
+        {
+            var secretKey = ConfigurationManager.AppSettings[ConfigToken];
+
+            if (string.IsNullOrWhiteSpace(secretKey))
+            {
+                if (secretKey.Length < 32 || secretKey.Length > 128)
+                {
+                    var message = string.Format(CultureInfo.CurrentCulture, "Could not find a valid configuration for WebHook receiver '{0}'. The setting must be set to a value between {1} and {2} characters long.", ConfigToken, 32, 128);
+                    request.GetConfiguration().DependencyResolver.GetLogger().Error(message);
+                    var noSecret = request.CreateErrorResponse(HttpStatusCode.InternalServerError, message);
+                    throw new HttpResponseException(noSecret);
+                }
+            }
+
+            return secretKey;
         }
     }
 }
